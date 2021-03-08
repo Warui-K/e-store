3c3883c9f8db97cba806d8715db7f783bf381ad0
Warui-K <warui.kamiri@gmail.com>
2021-03-08 11:16:59 +0300

    Basic Automated Testing using chai and eslint

---
+++ tests/server-running.js
@@ -0,0 +1,23 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index')
+
+//configure chai
+
+chai.use(chaiHttp)
+chai.should()
+
+describe("Make Sure that status is 200", function () {
+    it("should return 200", (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('object')
+                done()
+            }
+    )
+})
+
+})
+
