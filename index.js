049beff1ac17fd4ffe4815de53cf3a3f4258e6e9
Warui-K <warui.kamiri@gmail.com>
2021-03-10 10:30:07 +0300

    User model with the registration page setup

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const User = require('./models/User')
 require('./utils/db.config')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
-    try{
+    try {
         return res.render('index');
     }
-    catch (e){
+    catch (e) {
         return res.status(500).send('Internal Server Error')
     }
 });
+
+app.get('/register', function (req, res) {
+    try {
+        return res.render('register');
+    }
+    catch (e) {
+        return res.status(500).send('Internal Server Error')
+    }
+});
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save()
+    return res.render('register', { message: 'Registration successful' })
+})
 
 app.listen(3000, function () {
     console.log("Server running at port 3000");
 })
 
 module.exports = app
- 
